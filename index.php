674892d49fa99b52febdfda86f990b1e843c3ed1
Reid Carlberg <reid.carlberg@salesforce.com>
2013-04-03 16:40:23 -0500

    updated angular project name

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 if(file_exists('../local.settings.php'))
     include '../local.settings.php'; 
 ?>
-<html ng-app="project">
+<html ng-app="AngularSFDemo">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.js"></script>
     <script type="text/javascript">
-		var app = angular.module('project', ['AngularForce', 'AngularForceObjectFactory', 'Contact']);
+		var app = angular.module('AngularSFDemo', ['AngularForce', 'AngularForceObjectFactory', 'Contact']);
 
 		app.constant('SFConfig', {'sfLoginURL': 'https://login.salesforce.com/',
 	    'consumerKey': '<?= $_ENV['client_id'] ?>',
