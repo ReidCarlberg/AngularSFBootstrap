d1c8f99cd9406d35da37d79613cf737769f9f3c4
Reid Carlberg <reid.carlberg@salesforce.com>
2013-04-03 16:29:32 -0500

    well those shouldn't be there

--- local.settings.php
+++
@@ -1,6 +0,0 @@
-<?
-	//where we set the local callback, client ID, etc.
-	$_ENV['client_id']='3MVG9y6x0357HlefwuOOB3c0LW3JmId0TwBm.pe4XU6l1snC0odx.6npZmFeChvh6mdlG31EJpxoi48vSoGOC';
-	$_ENV['redirect_uri']='https://localhost/AngularSFBootstrap/#/callback';
-	$_ENV['proxy_url']='https://localhost/AngularSFBootstrap/proxy.php?mode=native';
-?>
